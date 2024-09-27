d8ff9231c541ec0dc96c363d2f7920214802801f
paper-piper <113584610+paper-piper@users.noreply.github.com>
2024-09-27 10:38:10 +0300

    made server and client classes instead of using globals, for some reason when migrating to bigger numbers the client can't find the hash (not a computing problem)

--- client.py
+++ client.py
@@ -19,134 +19,136 @@ logging.basicConfig(filename='client.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def start_client(client_socket):
-    """
-    Starts the client, sends CPU core information to the server, and handles the hash computation.
+class Client:
 
-    :param client_socket: The socket object for the client connection.
-    :return: None
-    """
-    try:
-        # Get the number of CPU cores
-        cpu_cores_num = os.cpu_count()
+    def __init__(self, client_socket, cpu_cores_num):
+        self.client_socket = client_socket
+        self.cpu_cores_num = cpu_cores_num
 
-        # Send the start connection message with the CPU cores as the parameter
-        start_message = protocol.encode_protocol(protocol.START_CONNECTION, str(cpu_cores_num))
-        protocol.send_message(client_socket, start_message)
+    def start_client(self):
+        """
+        Starts the client, sends CPU core information to the server, and handles the hash computation.
+        :return: None
+        """
+        try:
+            # Send the start connection message with the CPU cores as the parameter
+            start_message = protocol.encode_protocol(protocol.START_CONNECTION, str(self.cpu_cores_num))
+            protocol.send_message(self.client_socket, start_message)
 
-        # Get the range and desired hash results
-        raw_message = protocol.receive_message(client_socket)
-        msg_type, msg_params = protocol.decode_protocol(raw_message)
+            # Get the range and desired hash results
+            raw_message = protocol.receive_message(self.client_socket)
+            msg_type, msg_params = protocol.decode_protocol(raw_message)
 
-        if msg_type == protocol.SEND_DETAILS:
-            # before starting to compute hash, set a thread which will listen for quit message
-            threading.Thread(target=wait_for_quit, args=(client_socket,)).start()
+            if msg_type == protocol.SEND_DETAILS:
+                # before starting to compute hash, set a thread which will listen for quit message
+                threading.Thread(target=self.wait_for_quit).start()
 
-            results = handle_threads(cpu_cores_num, *msg_params)
+                results = self.handle_threads(*msg_params)
 
-            if results != "-1":  # Found the hash
-                message = protocol.encode_protocol(protocol.FOUND, ORIGINAL_HASH)
-                protocol.send_message(client_socket, message)
-                logging.info(f"Found hash! {results}")
-            else:  # Didn't find hash
-                message = protocol.encode_protocol(protocol.NOT_FOUND, results)
-                protocol.send_message(client_socket, message)
-                logging.info(f"Didn't find hash, searched from {msg_params[1]} to {msg_params[2]}")
+                if results != "-1":  # Found the hash
+                    message = protocol.encode_protocol(protocol.FOUND, ORIGINAL_HASH)
+                    protocol.send_message(self.client_socket, message)
+                    logging.info(f"Found hash! {results}")
+                else:  # Didn't find hash
+                    message = protocol.encode_protocol(protocol.NOT_FOUND, results)
+                    protocol.send_message(self.client_socket, message)
+                    logging.info(f"Didn't find hash, searched from {msg_params[1]} to {msg_params[2]}")
 
-    except ValueError as e:
-        logging.error(f"Value error: {e}")
-    except socket.error as e:
-        logging.error(f"Socket error: {e}")
-    finally:
-        # Close the connection
-        client_socket.close()
+        except ValueError as e:
+            logging.error(f"Value error: {e}")
+        except socket.error as e:
+            logging.error(f"Socket error: {e}")
+        finally:
+            # Close the connection
+            self.client_socket.close()
 
-
-def wait_for_quit(client_socket):
-    """
-    check if the server shuts down hash work or if the hash had been found by the client
-    :param client_socket:
-    :return:
-    """
-    while not stop_work.is_set():
-        raw_data = protocol.receive_message(client_socket)
-        msg_type, params = protocol.decode_protocol(raw_data)
-        if msg_type == protocol.STOP_WORK:
-            logging.info("Got quit message from server, stopping work")
-            stop_work.set()
+    def wait_for_quit(self):
+        """
+        check if the server shuts down hash work or if the hash had been found by the client
+        :return:
+        """
+        try:
+            while not stop_work.is_set():
+                raw_data = protocol.receive_message(self.client_socket)
+                msg_type, params = protocol.decode_protocol(raw_data)
+                if msg_type == protocol.STOP_WORK:
+                    logging.info("Got quit message from server, stopping work")
+                    stop_work.set()
+                    return
+        except socket.error as se:
+            # the socket was closed due to program finished, so ignore the error
             return
 
+    def handle_threads(self, desired_hash, range_start, range_end):
+        """
+        Handles the creation and management of threads for hash computation.
 
-def handle_threads(num_of_cores, desired_hash, range_start, range_end):
-    """
-    Handles the creation and management of threads for hash computation.
+        :param desired_hash: The target hash to be matched.
+        :param range_start: The start of the number range to search.
+        :param range_end: The end of the number range to search.
+        :return: The original hash if found, "-1" otherwise.
+        """
+        try:
+            range_start = int(range_start)
+            range_end = int(range_end)
+            capacity = range_end - range_start
+            capacity_per_thread = capacity // self.cpu_cores_num
+            threads = []
 
-    :param num_of_cores: Number of CPU cores available for threading.
-    :param desired_hash: The target hash to be matched.
-    :param range_start: The start of the number range to search.
-    :param range_end: The end of the number range to search.
-    :return: The original hash if found, "-1" otherwise.
-    """
-    try:
-        range_start = int(range_start)
-        range_end = int(range_end)
-        capacity = range_end - range_start
-        capacity_per_thread = capacity // num_of_cores
-        threads = []
+            for i in range(self.cpu_cores_num):
+                thread_start_range = range_start + i * capacity_per_thread
+                thread_end_range = range_start + (i + 1) * capacity_per_thread
+                thread = threading.Thread(target=self.compute_hash, args=(desired_hash,
+                                                                          thread_start_range,
+                                                                          thread_end_range))
+                threads.append(thread)
 
-        for i in range(num_of_cores):
-            thread_start_range = range_start + i * capacity_per_thread
-            thread_end_range = range_start + (i + 1) * capacity_per_thread
-            thread = threading.Thread(target=compute_hash, args=(desired_hash, thread_start_range, thread_end_range))
-            threads.append(thread)
+            # Start the threads
+            for thread in threads:
+                thread.start()
 
-        # Start the threads
-        for thread in threads:
-            thread.start()
+            # Wait for all threads to finish
+            for thread in threads:
+                thread.join()
 
-        # Wait for all threads to finish
-        for thread in threads:
-            thread.join()
+            # Return results
+            return ORIGINAL_HASH
+        except ValueError as e:
+            logging.error(f"Value error in range parsing: {e}")
+            return "-1"
 
-        # Return results
-        return ORIGINAL_HASH
-    except ValueError as e:
-        logging.error(f"Value error in range parsing: {e}")
-        return "-1"
+    def compute_hash(self, desired_hash, range_start, range_end):
+        """
+        Computes the hash for a range of numbers and compares it with the desired hash.
 
+        :param desired_hash: The target hash to be matched.
+        :param range_start: The start of the number range.
+        :param range_end: The end of the number range.
+        :return: None
+        """
+        global ORIGINAL_HASH
+        try:
+            # Iterate over the range from start to end
+            for num in range(range_start, range_end + 1):
+                # Check if the event has been set by another thread
+                if stop_work.is_set():
+                    return
+                # Pad the number according to the hash length (e.g., 5 -> "00005" if length is 5)
+                padded_str = str(num).zfill(HASH_LENGTH)
 
-def compute_hash(desired_hash, range_start, range_end):
-    """
-    Computes the hash for a range of numbers and compares it with the desired hash.
+                # Compute the MD5 hash for the padded string
+                hash_obj = hashlib.md5(padded_str.encode())
+                hash_guess = hash_obj.hexdigest()
 
-    :param desired_hash: The target hash to be matched.
-    :param range_start: The start of the number range.
-    :param range_end: The end of the number range.
-    :return: None
-    """
-    global ORIGINAL_HASH
-    try:
-        # Iterate over the range from start to end
-        for num in range(range_start, range_end + 1):
-            # Check if the event has been set by another thread
-            if stop_work.is_set():
+                # Check if the generated hash matches the desired hash
+                if hash_guess == desired_hash:
+                    stop_work.set()  # Signal that the solution is found
+                    ORIGINAL_HASH = str(num)  # Return the number if hash matches
+                    logging.info(f"Desired hash found: {padded_str} -> {hash_guess}")
+
                 return
-            # Pad the number according to the hash length (e.g., 5 -> "00005" if length is 5)
-            padded_str = str(num).zfill(HASH_LENGTH)
-
-            # Compute the MD5 hash for the padded string
-            hash_obj = hashlib.md5(padded_str.encode())
-            hash_guess = hash_obj.hexdigest()
-
-            # Check if the generated hash matches the desired hash
-            if hash_guess == desired_hash:
-                stop_work.set()  # Signal that the solution is found
-                ORIGINAL_HASH = str(num)  # Return the number if hash matches
-                logging.info(f"Desired hash found: {padded_str} -> {hash_guess}")
-
-            return
-    except Exception as e:
-        logging.error(f"Error during hash computation: {e}")
+        except Exception as e:
+            logging.error(f"Error during hash computation: {e}")
 
 
 def main():
@@ -158,7 +160,9 @@ def main():
     try:
         # Connect to the server
         client_socket.connect((SERVER_HOST, SERVER_PORT))
-        start_client(client_socket)
+        cpu_cores_num = os.cpu_count()
+        client = Client(client_socket, cpu_cores_num)
+        client.start_client()
     except socket.error as e:
         logging.critical(f"Failed to connect to the server: {e}")
     finally:
--- server.py
+++ server.py
@@ -2,26 +2,14 @@ import socket
 import threading
 import protocol
 import logging
-from datetime import datetime, timedelta
 
 # Global constants
 HOST = '127.0.0.1'  # Localhost
 PORT = 12345  # Port to listen on
-HASH_RESULTS = "f899139df5e1059396431415e770c6dd"
-HASH_LENGTH = 3
 
-# Client list to store connected clients
-clients = []
-id_count = 1
-
-# Hash variables
-MAX_HASH = 999
-shared_ranges = 0
-CPU_POWER = 5
-
-# Global configurations
-TIMEOUT = 5  # seconds
-
+HASH_RESULTS = "04fc711301f3c784d66955d98d399afb"
+HASH_LENGTH = 6
+CPU_POWER = 500
 # Configure logging
 logging.basicConfig(filename='server.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
@@ -39,126 +27,132 @@ class Client:
         self.client_id = client_id
         self.cpu_cores = cpu_cores
         self.client_socket = client_socket
-        self.start_work_time = datetime.now()
 
 
-def handle_client(client_socket, client_address):
-    """
-    Handles a new client connection, receives messages, and processes their hash request.
+class Server:
 
-    :param client_socket: The socket object for the client connection.
-    :param client_address: The address of the connected client.
-    :return: None
-    """
-    global id_count
-    logging.info(f"New connection from {client_address}")
+    def __init__(self, server_socket, cpu_power):
+        self.server_socket = server_socket
+        # Hash variables
+        self.max_hash = (10 ** HASH_LENGTH) - 1
+        self.current_hash = 0
+        self.cpu_power = cpu_power
+        self.lock = threading.Lock()
+        # Client list to store connected clients
+        self.clients = []
+        self.id_count = 1
 
-    try:
-        # Receive the start connection message
-        raw_message = protocol.receive_message(client_socket)
-        msg_type, msg_params = protocol.decode_protocol(raw_message)
+    def handle_client(self, client_socket, client_address):
+        """
+        Handles a new client connection, receives messages, and processes their hash request.
 
-        if msg_type == protocol.START_CONNECTION:
-            # Client wants to connect, extract the number of cores
-            cpu_cores = int(msg_params[0])
-            client_id = id_count
-            id_count += 1
-            logging.info(f"Client with id {client_id} has {cpu_cores} cores.")
+        :param client_socket: The socket object for the client connection.
+        :param client_address: The address of the connected client.
+        :return: None
+        """
+        logging.info(f"New connection from {client_address}")
 
-            # Create a new client object and add to the list of clients
-            client = Client(client_id, cpu_cores, client_socket)
-            clients.append(client)
-            logging.info(f"Client added: {client.client_id}, Total clients: {len(clients)}")
-            send_details_message(client)
-
-            # Wait for results
+        try:
+            # Receive the start connection message
             raw_message = protocol.receive_message(client_socket)
             msg_type, msg_params = protocol.decode_protocol(raw_message)
 
-            if msg_type == protocol.FOUND:
-                logging.info(f"Found the hash! ({msg_params[0]})")
-                # let all other client's know the work is over
-                message = protocol.encode_protocol(protocol.STOP_WORK)
-                for other_client in clients:
-                    if other_client != client:
-                        protocol.send_message(other_client.client_socket, message)
+            if msg_type == protocol.START_CONNECTION:
+                # Client wants to connect, extract the number of cores
+                cpu_cores = int(msg_params[0])
+                with self.lock:
+                    client_id = self.id_count
+                    self.id_count += 1
+                    logging.info(f"Client with id {client_id} has {cpu_cores} cores.")
 
-            elif msg_type == protocol.NOT_FOUND:
-                logging.info(f"Client {client.client_id} didn't find the hash")
+                    # Create a new client object and add to the list of clients
+                    client = Client(client_id, cpu_cores, client_socket)
+                    self.clients.append(client)
+                logging.info(f"Client added: {client.client_id}, Total clients: {len(self.clients)}")
+                self.send_details_message(client)
 
-    except ValueError as e:
-        logging.error(f"Value error in message parsing: {e}")
-    except socket.error as e:
-        logging.error(f"Socket error with client {client_address}: {e}")
-    finally:
-        client_socket.close()
+                # Wait for results
+                raw_message = protocol.receive_message(client_socket)
+                msg_type, msg_params = protocol.decode_protocol(raw_message)
 
+                if msg_type == protocol.FOUND:
+                    logging.info(f"Found the hash! ({msg_params[0]})")
+                    # let all other client's know the work is over
+                    message = protocol.encode_protocol(protocol.STOP_WORK)
+                    for other_client in self.clients:
+                        if other_client != client:
+                            protocol.send_message(other_client.client_socket, message)
 
-def check_for_timeout():
-    pass
+                elif msg_type == protocol.NOT_FOUND:
+                    logging.info(f"Client {client.client_id} didn't find the hash")
 
+        except ValueError as e:
+            logging.error(f"Value error in message parsing: {e}")
+        except socket.error as e:
+            logging.error(f"Socket error with client {client_address}: {e}")
+        finally:
+            client_socket.close()
 
-def send_details_message(client):
-    """
-    Sends the hash range details to the client for processing.
-    :param client: The client object to send details to.
-    :return: None
-    """
-    range_start, range_end = get_hash_range(client.cpu_cores)
-    message = protocol.encode_protocol(protocol.SEND_DETAILS, HASH_RESULTS, str(range_start), str(range_end))
-    protocol.send_message(client.client_socket, message)
+    def send_details_message(self, client):
+        """
+        Sends the hash range details to the client for processing.
+        :param client: The client object to send details to.
+        :return: None
+        """
+        range_start, range_end = self.get_hash_range(client.cpu_cores)
+        message = protocol.encode_protocol(protocol.SEND_DETAILS, HASH_RESULTS, str(range_start), str(range_end))
+        protocol.send_message(client.client_socket, message)
 
+    def get_hash_range(self, cores_number):
+        """
+        Calculates the hash range that a client should process based on its CPU cores.
 
-def get_hash_range(cores_number):
-    """
-    Calculates the hash range that a client should process based on its CPU cores.
+        :param cores_number: The number of CPU cores the client has.
+        :return: Tuple containing the start and end of the hash range.
+        """
+        with self.lock:
+            # if the whole range is covered, check form 0-0
+            if self.current_hash > self.max_hash:
+                return 0, 0
+            capacity = cores_number * CPU_POWER
+            start_range = self.current_hash
+            end_range = start_range + capacity
+            end_range = self.max_hash if end_range > self.max_hash else end_range
 
-    :param cores_number: The number of CPU cores the client has.
-    :return: Tuple containing the start and end of the hash range.
-    """
-    global shared_ranges
-    # if the whole range is covered, check form 0-0
-    if shared_ranges > MAX_HASH:
-        return 0, 0
-    capacity = cores_number * CPU_POWER
-    start_range = shared_ranges
-    end_range = start_range + capacity
-    end_range = MAX_HASH if end_range > MAX_HASH else end_range
+            # update ranges
+            self.current_hash = end_range
+            return start_range, end_range
 
-    # update ranges
-    shared_ranges = end_range
-    return start_range, end_range
+    def start_server(self):
+        """
+        Starts the server, listens for incoming connections, and handles clients in separate threads.
 
+        :return: None
+        """
+        try:
 
-def start_server():
-    """
-    Starts the server, listens for incoming connections, and handles clients in separate threads.
+            # Start listening for incoming connections
+            self.server_socket.listen()
+            logging.info(f"Server listening on {HOST}:{PORT}")
 
-    :return: None
-    """
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        server_socket.bind((HOST, PORT))
+            while True:
+                try:
+                    # Accept a new client connection
+                    client_socket, client_address = self.server_socket.accept()
 
-        # Start listening for incoming connections
-        server_socket.listen()
-        logging.info(f"Server listening on {HOST}:{PORT}")
-
-        while True:
-            try:
-                # Accept a new client connection
-                client_socket, client_address = server_socket.accept()
-
-                # Handle each client in a separate thread
-                client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
-                client_thread.start()
-            except socket.error as e:
-                logging.error(f"Error accepting new client: {e}")
-    except socket.error as e:
-        logging.critical(f"Failed to start the server: {e}")
-    finally:
-        server_socket.close()
+                    # Handle each client in a separate thread
+                    client_thread = threading.Thread(target=self.handle_client, args=(client_socket, client_address))
+                    client_thread.start()
+                except socket.error as e:
+                    logging.error(f"Error accepting new client: {e}")
+        except socket.error as e:
+            logging.critical(f"Failed to start the server: {e}")
+        finally:
+            self.server_socket.close()
 
 
 if __name__ == "__main__":
-    start_server()
+    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.bind((HOST, PORT))
+    server = Server(socket, CPU_POWER)
+    server.start_server()
